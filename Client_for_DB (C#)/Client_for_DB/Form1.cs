7a3933dd770e04ae63f621fa522a0edef2c8e736
Bull6 <d.i.taigin@gmail.com>
2021-04-21 00:36:12 +0500

    Create Form1.cs

---
+++ Client_for_DB (C#)/Client_for_DB/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client_for_DB
+{
+    public partial class ConnectForm : Form
+    {
+        public ConnectForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_connect_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
