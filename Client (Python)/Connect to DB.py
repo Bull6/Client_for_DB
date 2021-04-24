db56b1573d0cb45d2acaadecda7ea3940a9fd075
Bull6 <d.i.taigin@gmail.com>
2021-04-24 11:52:07 +0500

    Read to dataframe

--- Client (Python)/Connect to DB.py
+++ Client (Python)/Connect to DB.py
@@ -1,5 +1,11 @@
+import pandas as pd
+import urllib
+
 import pyodbc
 from datetime import datetime
+
+#import mssql
+from sqlalchemy import create_engine
 
 '''class Sql:
     def __init__(self, database, server="192.168.1.202,1433"):
@@ -19,12 +25,18 @@ db = 'DATABASE=ElectroTransport'
 user = 'UID=sa'
 pw = 'PWD=290798Denis'
 conn_str = ';'.join([driver, server, port, db, user, pw])
-
+params = urllib.parse.quote_plus(conn_str)
+engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
+'''
 conn = pyodbc.connect(conn_str)
 cursor = conn.cursor()
 
-print(cursor.execute('select Depo_City from Depo'))
+print(cursor.execute('select * from Depo'))
 row = cursor.fetchone()
 rest_of_rows = cursor.fetchall()
 print (row)
 print (rest_of_rows)
+'''
+qry = """select * from Depo"""
+df = pd.read_sql(qry, engine)
+print(df.head())
